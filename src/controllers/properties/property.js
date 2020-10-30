3e6ce4b84b4281858e272039fa3f99d0e9a74c70
godofjs <henryeze019@gmail.com>
2020-10-30 16:10:33 +0000

    set up for deployment

--- src/controllers/properties/property.js
+++ src/controllers/properties/property.js
@@ -1,21 +1,36 @@
 const base = require("../../base");
 const Property = require("../../models/property")
 module.exports.addProperty = async (res) => {
-    const { type, size, noOfRooms, price, state, landmark, location, images, ownerId, extraComment, furnishing,bathrooms,condition,parkingSpace } = res.body;
+    const { type, size, noOfRooms, price, state, landmark, location, images, ownerId, extraComment, propertyId, furnishing, bathrooms, condition, parkingSpace } = res.body;
     // checks for the local variables of the function
     if (!ownerId) throw new base.ResponseError(400, "Owner of property must be provided.");
     if (!size) throw new base.ResponseError(400, "Size of property is a compulsory field.");
     if (!price) throw new base.ResponseError(400, "Price of property was not provided.");
     if (images.length === 0) throw new base.ResponseError(400, "Image of property was not provided.");
     if (!state) throw new base.ResponseError(400, "State was not provided.");
-    if (!location ) throw new base.ResponseError(400, "Location  was not provided.");
+    if (!location) throw new base.ResponseError(400, "Location  was not provided.");
     if (!location.country) throw new base.ResponseError(400, "Country  was not provided. try selecting from the locations field.");
     if (!type) throw new base.ResponseError(400, "Provide type of property.");
     if (!landmark) throw new base.ResponseError(400, "Provide a landmark for user to locate property.");
-    const property = new Property({ type, size, noOfRooms, createdAt: Date.now(), price, state, landmark, location, images, ownerId, extraComment , furnishing, bathrooms, condition, parkingSpace });
-    await property.save().catch(e => { throw new base.ResponseError(400, e.message)})
+    let property, message;
+    if (propertyId) {
+        property = await Property.find({ _id: propertyId });
+        await Property.updateOne(
+            { _id: propertyId },
+            {
+                $set: { type, size, noOfRooms, price, state, landmark, location, images, ownerId, extraComment, furnishing, bathrooms, condition, parkingSpace },
+                $currentDate: { "lastModified": true }
+            })
+        property = await Property.find({ _id: propertyId });
+        message = "Property edited successfully"
+    }
+    else {
+        message = "Property added successfully"
+        property = new Property({ type, size, noOfRooms, createdAt: Date.now(), price, state, landmark, location, images, ownerId, extraComment, furnishing, bathrooms, condition, parkingSpace });
+    }
+    // await property.save().catch(e => { throw new base.ResponseError(400, e.message)})
     return new base.Response(201, {
-        message: "Property added successfully",
+        message: message,
         error: false
     })
 }
@@ -32,19 +47,8 @@ module.exports.getProperties = async (request) => {
 module.exports.getProperty = async (request) => {
     const id = request.params.id;
     if (!id) throw new base.ResponseError(400, "Id of user must be provided.")
-    const property = await Property.findOne({_id: id});
-    if(!property) throw new base.ResponseError(404, "Property not found")
-    return new base.Response(201, {
-        message: "Property found",
-        error: false,
-        data: property
-    })
-}
-module.exports.editProperty = async (request) => {
-    const id = request.params.id;
-    if (!id) throw new base.ResponseError(400, "Id of user must be provided.")
-    const property = await Property.findOne({_id: id});
-    if(!property) throw new base.ResponseError(404, "Property not found")
+    const property = await Property.findOne({ _id: id });
+    if (!property) throw new base.ResponseError(404, "Property not found")
     return new base.Response(201, {
         message: "Property found",
         error: false,
